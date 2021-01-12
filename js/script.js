cb2f07826d0801243b78be9d395bbfd83085dafd
alessandro <finialessandro90@gmail.com>
2021-01-12 17:55:59 +0100

    bonus angle down/up

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 $(document).ready(function(){
 
     $('.with-dropdown').hover(function(){
+
         $(this).children('.dropdown-menu').toggleClass('active');
+
+        $(this).find('.ico-down').toggle();
+
+        $(this).find('.ico-up').toggle();
+
+        $(this).children('a').toggleClass('black');
+
     });
 
 });
